8282009e788c5ac5724f8e9b975fc7032c95a33f
sangsq <sang93@qq.com>
2024-04-23 13:15:01 +0800

    feat: 命令参数解析迁移到System.CommandLine库

--- SangServerTool/Options.cs
+++ SangServerTool/Options.cs
@@ -1,30 +1,24 @@
-﻿using CommandLine;
-
-namespace SangServerTool
+﻿namespace SangServerTool
 {
 
     /// <summary>
     /// 请求SSL证书的参数
     /// </summary>
-    [Verb("ssl", HelpText = "Get Let's Encrypt SSL Cert.")]
     public class AUTO_SSL
     {
         /// <summary>
         /// 配置ASSK和证书信息
         /// </summary>
-        [Option('c', "config", Required = true, HelpText = "Set config json file.")]
         public string? ConfigFile { get; set; }
 
         /// <summary>
         /// DNS验证重试多少次？
         /// </summary>
-        [Option("retry", Default = 8, HelpText = "How many retries? ")]
         public int Retry { get; set; }
 
         /// <summary>
         /// DNS验证失败等待多少秒重试？
         /// </summary>
-        [Option("delay", Default = 5, HelpText = "How many seconds to retry? ")]
         public int Delay { get; set; }
     }
 
@@ -32,37 +26,31 @@ namespace SangServerTool
     /// <summary>
     /// 配置DDNS的参数
     /// </summary>
-    [Verb("ddns", HelpText = "Set DDNS.")]
     public class AUTO_DDNS
     {
         /// <summary>
         /// 配置AKSK相关,及DDNS域
         /// </summary>
-        [Option('c', "config", Required = true, HelpText = "Set config json file.")]
         public string? ConfigFile { get; set; }
 
         /// <summary>
         /// 延迟多少秒执行
         /// </summary>
-        [Option("delay", Default = 0, HelpText = "How many seconds delay?")]
         public int Delay { get; set; }
 
         /// <summary>
         /// 是否为IPv6地址
         /// </summary>
-        [Option("del", Default = false, HelpText = "Is delete DDNS?")]
         public bool Del { get; set; }
 
         /// <summary>
         /// 是否为IPv6地址
         /// </summary>
-        [Option("v6", Default = false, HelpText = "Is ipv6?")]
         public bool IPV6 { get; set; }
 
         /// <summary>
         /// 指定IP
         /// </summary>
-        [Option("ip", Default = "", HelpText = "If set will be used. Otherwise automatically obtained.\n You can set 'ifconfig', It will check from 'https://ifconfig.me/ip' to get you Internet IP.")]
         public string? IP { get; set; }
 
 
--- SangServerTool/Program.cs
+++ SangServerTool/Program.cs
@@ -1,8 +1,9 @@
-﻿using CommandLine;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SangServerTool;
 using SangServerTool.Tool;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
 
 
 ServiceCollection services = new();
@@ -17,18 +18,36 @@ services.AddLogging(logBuilder => {
 using var sp = services.BuildServiceProvider();
 ILoggerFactory loggerFactory = sp.GetService<ILoggerFactory>();
 
-return await Parser.Default.ParseArguments<AUTO_DDNS, AUTO_SSL>(args)
-.MapResult(
-async (AUTO_DDNS opt) => {
+// 创建根命令
+var rootCommand = new RootCommand("SangServerTool");
+
+// 定义ssl命令
+var sslCommand = new Command("ssl", "Get Let's Encrypt SSL Cert.");
+sslCommand.AddOption(new Option<string>(new[]{"--config", "-c"}, "Set config json file.") { IsRequired = true });
+sslCommand.AddOption(new Option<int>("--retry", () => 8, "How many retries?"));
+sslCommand.AddOption(new Option<int>("--delay", () => 5, "How many seconds to retry?"));
+sslCommand.Handler = CommandHandler.Create<string, int, int>(async (config, retry, delay) =>
+{
+    var opt = new AUTO_SSL { ConfigFile = config, Retry = retry, Delay = delay };
+    var logger = loggerFactory.CreateLogger("SangServerTool_SSL");
+    return await SSL.Run(opt, logger);
+});
+rootCommand.AddCommand(sslCommand);
+
+// 定义ddns命令
+var ddnsCommand = new Command("ddns", "Set DDNS.");
+ddnsCommand.AddOption(new Option<string>(new[] { "--config", "-c" }, "Set config json file.") { IsRequired = true});
+ddnsCommand.AddOption(new Option<int>("--delay", () => 0, "How many seconds delay?"));
+ddnsCommand.AddOption(new Option<bool>("--del", () => false, "Is delete DDNS?"));
+ddnsCommand.AddOption(new Option<bool>("--v6", () => false, "Is ipv6?"));
+ddnsCommand.AddOption(new Option<string>("--ip", () => "", "If set will be used. Otherwise automatically obtained.\n You can set 'ifconfig', It will check from 'https://ifconfig.me/ip' to get you Internet IP."));
+ddnsCommand.Handler = CommandHandler.Create<string, int, bool, bool, string>(async (config, delay, del, v6, ip) =>
+{
+    var opt = new AUTO_DDNS { ConfigFile = config, Delay = delay, Del = del, IPV6 = v6, IP = ip };
     var logger = loggerFactory.CreateLogger("SangServerTool_DDNS");
     return await DDNS.Run(opt, logger);
-},
-async (AUTO_SSL opt) => {
-    var logger = loggerFactory.CreateLogger("SangServerTool_SSL");
-    return await SSL.Run(opt, logger); 
-},
-async errs =>
-{
-    await Task.Delay(100);
-    return 1;
 });
+rootCommand.AddCommand(ddnsCommand);
+
+// 解析并执行命令
+return await rootCommand.InvokeAsync(args);
